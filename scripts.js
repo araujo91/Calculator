9903fab30d09259073a4cb7ab974ebea1ef41425
araujo91 <joaocsaraujo.91@gmail.com>
2020-08-20 20:43:38 +0100

    possible solution

--- scripts.js
+++ scripts.js
@@ -1,39 +1,195 @@
-function sum(a, b) {
-    return a+ b;
-}
 
-function sub(a, b) {
-    return a+ b;
-}
 
-function mul(a, b) {
-    return a+ b;
-}
+let gNr = [];
+let gI = 0;
+let gFl = 0;
+const btn = document.getElementById('buttons');
+const pOps = document.getElementById("ops");
+const pRes = document.getElementById("res");
+btn.addEventListener('click', function(e) {
+    let x = e.toElement.innerHTML;
+    let y = e.toElement.className;
+    console.log(y == "btn oper");
+    pOps.innerHTML += x;
+    else if(x == "="){
+        console.log(gNr)
+        gNr[gI] = parseFloat(gNr[gI]);
+        operate(gNr);
+    } else if(y == "btn_nr"){
+        if(gNr[gI] = "undefined"){gNr[gI] = 0}
+        gNr[gI] += x;
+    } else {
+        gNr[gI] = parseFloat(gNr[gI]);
+        gI++;
+        gNr[gI] = x;
+        gI++
+        }
+})
+       
 
-function div(a, b) {
-    return a+ b;
+function sum(array) {
+    array[2] = array[0] + array[2];
+    array.shift();
+    array.shift();
+    return array;
 }
-
-function operate(a, b, c) {
-    switch (c) {
+                
+function sub(array) {
+    array[2] = array[0] - array[2];
+    array.shift();
+    array.shift();
+    return array;
+}
+                
+function mul(array) {
+    array[2] = array[0] * array[2];
+    array.shift();
+    array.shift();
+    return array;
+}
+                
+function div(array) {
+    array[2] = array[0] / array[2];
+    array.shift();
+    array.shift();
+    return array;
+}
+                
+function operate(array) {
+                
+    switch (array[1]) {
         case '+':
-            console.log(sum(a, b));
+            sum(array);
+            pRes.innerHTML = array[0];
             break;
         case '-':
-            console.log(sub(a, b));
+            sub(array);
+            pRes.innerHTML = array[0];
             break;
         case '*':
-            console.log(mul(a, b));
+            mul(array);
+            pRes.innerHTML = array[0];
             break;
         case '/': 
-        console.log(div(a, b));
+            div(array);
+            pRes.innerHTML = array[0];
             break;
-    }
-}
+        }
+}               
 
-const btn = document.getElementById('buttons');
-                btn.addEventListener('click', function(e) {
+
+
+
+
+// let gNr = [];
+// let gI = 0;
+// let gFlo = 0;
+
+// const btn = document.getElementById('buttons');
+// const pOps = document.getElementById("ops");
+// const pRes = document.getElementById("res");
+// btn.addEventListener('click', function(e) {
+//     let x = e.toElement.innerHTML;
+//     let y = e.toElement.className;
+//     if(gNr[gI] = "undefined"){gNr[gI] = 0}
+//     if(x == ".") {        
+//         console.log("ponto introduzido")
+//         flo(x)
+//     } else if (y == "btn oper") {
+//         pOps.innerHTML += x;
+//         console.log("operador introduzido")
+//         oper(x)
+//         }
+//     else if(x == "="){
+//         console.log("resolv")
+//         gNr[gI] = parseFloat(gNr[gI]);
+//         operate(gNr);
+//     } else if(y == "btn_nr"){
+        
+//         nr(x)
+        
+//     // } else {
+//     //     gNr[gI] = parseFloat(gNr[gI]);
+//     //     gFlo--;
+//     //     gI++;
+//     //     gNr[gI] = x;
+//     //     gI++
+//     //     }
+// }})
+       
+
+// function sum(array) {
+//     array[2] = array[0] + array[2];
+//     array.shift();
+//     array.shift();
+//     return array;
+// }
                 
-                let x = console.log(e);})
-                //let y = console.log(e.originalTarget.className);})
-                //let y = console.log
+// function sub(array) {
+//     array[2] = array[0] - array[2];
+//     array.shift();
+//     array.shift();
+//     return array;
+// }
+                
+// function mul(array) {
+//     array[2] = array[0] * array[2];
+//     array.shift();
+//     array.shift();
+//     return array;
+// }
+                
+// function div(array) {
+//     array[2] = array[0] / array[2];
+//     array.shift();
+//     array.shift();
+//     return array;
+// }
+                
+// function operate(array) {
+                
+//     switch (array[1]) {
+//         case '+':
+//             sum(array);
+//             pRes.innerHTML = array[0];
+//             break;
+//         case '-':
+//             sub(array);
+//             pRes.innerHTML = array[0];
+//             break;
+//         case '*':
+//             mul(array);
+//             pRes.innerHTML = array[0];
+//             break;
+//         case '/': 
+//             div(array);
+//             pRes.innerHTML = array[0];
+//             break;
+//         }
+// }               
+
+// function flo(x) {
+//     if (gFlo == 0) {
+//         console.log("estou a meter")
+//         gNr[gI] += x;
+//         gFlo++;
+//         pOps.innerHTML += x;
+//     } else if(gFlo !== 0) {
+//         console.log("only 1 dot" );
+        
+//     }
+// }
+
+// function oper(x) {
+//     gNr[gI] = parseFloat(gNr[gI]);
+//         gFlo--;
+//         gI++;
+//         gNr[gI] = x;
+//         gI++
+// }
+
+// function nr(x){
+    
+//     pOps.innerHTML += x;
+//     gNr[gI] += x;
+// }
